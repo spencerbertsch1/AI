c8f3b5cdeb36fc36bee9eab562ff8532b3e185ff
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-09-19 16:30:52 -0400

    add get_successors method

--- ChickensAndFoxes/FoxProblem.py
+++ ChickensAndFoxes/FoxProblem.py
@@ -8,10 +8,80 @@ class FoxProblem:
         #  based on start_state
 
     # get successor states for the given state
-    def get_successors(self, state):
-        pass
-        # you write this part. I also had a helper function
-        # that tested if states were safe before adding to successor list
+    def get_successors(self, state: tuple) -> list:
+        """
+        Method to get the legal next states given a state. The boat can either be on the left or right bank and
+        can either ferry both a chicken and fox, or just a chicken, or just a fox to the other side of the river.
+
+        :param state: tuple (chicken, fox, boat)
+        :return: list of tuples representing the legal next states
+        """
+        successors: list = []
+
+        # define the number of chickens and foxes on each side of the river
+        left_chickens = state[0]
+        right_chickens: int = self.start_state[0] - state[0]
+        left_foxes: int = state[1]
+        right_foxes: int = self.start_state[1] - state[1]
+
+        if state[2] == 0:
+            print(f"Boat is on right bank - collecting the possible next states for {state}.")
+            if (right_chickens > 0) & (right_foxes > 0):
+                successors.append((state[0]+1, state[1]+1, state[2]+1))
+
+            if right_chickens > 0:
+                successors.append((state[0]+1, state[1], state[2]+1))
+
+            if right_foxes > 0:
+                successors.append((state[0], state[1]+1, state[2]+1))
+
+        elif state[2] == 1:
+            print(f"Boat is on left bank - collecting the possible next states for {state}.")
+            if(left_chickens > 0) & (left_foxes > 0):
+                successors.append((state[0]-1, state[1]-1, state[2]-1))
+
+            if left_chickens > 0:
+                successors.append((state[0]-1, state[1], state[2]-1))
+
+            if left_foxes > 0:
+                successors.append((state[0], state[1]-1, state[2]-1))
+
+        else:
+            raise Exception(f"Something has gone wrong... check state: {state}.")
+
+        # remove the elements of 'successors' that are illegal
+        for current_state in successors.copy():
+            is_legal = self.is_legal(state=current_state)
+            if not is_legal:
+                print(f"Illegal state found... Removing: {current_state}.")
+                successors.remove(current_state)
+
+        print(f'SUCCESSORS TO {state}: {successors}')
+        return successors
+
+    def is_legal(self, state: tuple) -> bool:
+        """
+        Method to determine whther or not a given state is legal or not based on the number of chickens and foxes
+        on each side of the river. If there are more foxes than chickens on either side, then the state is illegal
+
+        :param state: tuple (chickens, foxes, boat)
+        :return: True if legal, False otherwise.
+        """
+        legal: bool = True
+
+        # define the number of chickens and foxes on each side of the river
+        left_chickens = state[0]
+        right_chickens: int = self.start_state[0] - state[0]
+        left_foxes: int = state[1]
+        right_foxes: int = self.start_state[1] - state[1]
+
+        # if there are more foxes than chickens on either side, set legal to False
+        if (left_foxes > left_chickens) & (left_chickens != 0):
+            legal = False
+        elif (right_foxes > right_chickens) & (right_chickens != 0):
+            legal = False
+
+        return legal
 
     # I also had a goal test method. You should write one.
     def goal_test(self, state: tuple):
@@ -30,8 +100,8 @@ class FoxProblem:
         return string
 
 
-# A bit of test code
+# __main__ here just for testing - can be safely ignored or removed.
 if __name__ == "__main__":
-    test_cp = FoxProblem((5, 5, 1))
-    print(test_cp.get_successors((5, 5, 1)))
+    test_cp = FoxProblem(start_state=(3, 3, 1))
+    print(test_cp.get_successors(state=(2, 2, 0)))
     print(test_cp)
