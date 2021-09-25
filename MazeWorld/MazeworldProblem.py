d8edbd87518f3fcdaa310a053f074ea09e667fa3
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-09-25 13:05:15 -0400

    add get_successors function

--- MazeWorld/MazeworldProblem.py
+++ MazeWorld/MazeworldProblem.py
@@ -3,18 +3,40 @@ from time import sleep
 
 class MazeworldProblem:
 
-    ## you write the constructor, and whatever methods your astar function needs
+    # you write the constructor, and whatever methods your astar function needs
 
-    def __init__(self, maze, goal_locations):
-        pass
+    def __init__(self, maze, goal_locations: tuple):
+        self.maze = maze
+        self.goals = goal_locations
 
     def __repr__(self):
         string = "MazeWorld problem: "
         return string
 
-
         # given a sequence of states (including robot turn), modify the maze and print it out.
         #  (Be careful, this does modify the maze!)
+
+    def is_legal(self, all_possible_states: list) -> list:
+        legal_states: list = []
+        # given a list of states, remove the illegal ones
+        for state in all_possible_states:
+            if self.maze.is_floor(x=state[0], y=state[1]):
+                legal_states.append(state)
+            else:
+                print(f'Illegal state found - {state}')
+
+        return legal_states
+
+    def get_successors(self, state: tuple):
+        print(f'Finding potential next states for the current state: {state}')
+        print(f'Maze information: Width: {self.maze.width}, Height: {self.maze.height}')
+
+        all_successors: list = [(state[0]+1, state[1]), (state[0]-1, state[1]),
+                                (state[0], state[1]+1), (state[0], state[1]-1)]
+
+        return self.is_legal(all_possible_states=all_successors)
+
+
 
     def animate_path(self, path):
         # reset the robot locations in the maze
@@ -23,16 +45,18 @@ class MazeworldProblem:
         for state in path:
             print(str(self))
             self.maze.robotloc = tuple(state[1:])
-            sleep(1)
+            sleep(0.5)
 
             print(str(self.maze))
 
 
-## A bit of test code. You might want to add to it to verify that things
+# A bit of test code. You might want to add to it to verify that things
 #  work as expected.
-
 if __name__ == "__main__":
-    test_maze3 = Maze("maze3.maz")
-    test_mp = MazeworldProblem(test_maze3, (1, 4, 1, 3, 1, 2))
+    test_maze = Maze("maze_test.maz")
+    test_mp = MazeworldProblem(test_maze, (19, 2))
 
-    print(test_mp.get_successors((0, 1, 0, 1, 2, 2, 1)))
+    print(test_mp.get_successors((8, 8)))
+
+    # test_mp = MazeworldProblem(test_maze, (1, 4, 1, 3, 1, 2))  # <-- for testing multi robot, multi goal
+    # print(test_mp.get_successors((0, 1, 0, 1, 2, 2, 1)))
