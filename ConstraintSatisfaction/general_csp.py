e29b92c40782870bc33c820f75e6e982a138a323
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-10-14 16:45:14 -0400

    add some logic to the general CSP

--- ConstraintSatisfaction/general_csp.py
+++ ConstraintSatisfaction/general_csp.py
@@ -20,20 +20,25 @@ class CSP:
             f'Constraints: {self.c}'
         ])
 
-    def is_complete(self, assignment, csp) -> bool:
-        """
-        TODO determine whether or not the current assignment is a solution to the CSP
-        :param assignment:
-        :param csp:
-        :return:
-        """
-        return False
-
     @ staticmethod
     def select_unassigned_variable(csp):
         # maybe we should remove var from the unassigned variables here
+        # TODO wouldn't it be easier to just pass the assignment into this function instead of keeping track in the CSP?
+
         val = list(csp.unassigned_variables)[1]
         return val
+
+    def revise(self, csp, X, Y):
+        """
+        Helper function for AC-3 function
+
+        TODO implement revise
+        :param csp:
+        :param X:
+        :param Y:
+        :return:
+        """
+        pass
 
     def AC3(self, csp):
         """
@@ -55,7 +60,34 @@ class CSP:
         # TODO is this right?? Where do we use the assignment here?
         return domains
 
-    def test_consistency(self):
+    def test_consistency(self, assignment, csp) -> bool:
+        """
+        returns True if the assignment is consistent and doesn't violate any constraints.
+        returns False otherwise.
+
+        :param assignment:
+        :param csp:
+        :return: bool
+        """
+        # TODO how should we do this? We could iterate over each k-v pair in the assignment dict and then
+        # TODO iterate over all the constraints to ensure they never match, seems costly but maybe that's the best move
+        answer = True
+        for var, value in assignment.items():
+            for constraint in csp.c:
+                if ((var == constraint[0]) & (value == constraint[1])) | \
+                   ((var == constraint[1]) & (value == constraint[0])):
+                    answer = False
+
+        return answer
+
+    def inference(self, csp, var, value):
+        """
+        TODO
+        :param csp:
+        :param var:
+        :param value:
+        :return:
+        """
         pass
 
     def backtracking_search(self, csp):
@@ -69,8 +101,8 @@ class CSP:
         :param csp:
         :return: either a valid assignment or False if none exists
         """
-        # if assignment is complete, then we return the assignment
-        if self.is_complete(assignment=assignment, csp=csp):
+        # if assignment is complete, then we return the assignment here
+        if set(assignment) == set(csp.x):  # TODO <-- double check that this works
             return assignment
 
         var = self.select_unassigned_variable(csp=csp)
@@ -78,10 +110,29 @@ class CSP:
 
         for value in self.order_domain_values(var=var, assignment=assignment, csp=csp):
             # test whether or not the value is consistent with the assignment
-            pass
-                # add value to assignment
+            print(f'Testing whether or not {value} is consistent with the assignment: {assignment}')
 
-                # see if any constraints are violated
+            # add value to test assignment
+            test_assignment = assignment
+            test_assignment[var] = value
+
+            # see if any constraints are violated
+            if self.test_consistency(assignment=test_assignment, csp=csp):
+
+                # add value to assignment
+                assignment[var] = value
+
+                # # TODO add inference code later...
+                # inferences = self.inference(csp=csp, var=var, value=value)
+                # if inferences:
+                result = self.backtrack(assignment, csp)
+
+                if result is not False:
+                    return result
+
+            del assignment[var]  # <-- TODO maybe this shouldn't be here?... but maybe it should...
+
+        return False
 
 
 # some test code - this section can be safely ignored or removed.
@@ -91,23 +142,14 @@ if __name__ == "__main__":
     x = {'WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T'}
 
     # define the domains
-    d = {'WA': {'red', 'green', 'blue'},
-         'NT': {'red', 'green', 'blue'},
-         'Q': {'red', 'green', 'blue'},
-         'NSW': {'red', 'green', 'blue'},
-         'V': {'red', 'green', 'blue'},
-         'SA': {'red', 'green', 'blue'},
-         'T': {'red', 'green', 'blue'}
-         }
+    doms = ['red', 'green', 'blue']
+    # we might need something like this in the future vvv
+    d = {'WA': doms, 'NT': doms, 'Q': doms, 'NSW': doms, 'V': doms, 'SA': doms, 'T': doms}
 
     # define the constraints
     # write a function that takes 2 countries and their colors and returns True if that is ok, False if it's illegal
     c = [('SA', 'WA'), ('SA', 'NT'), ('SA', 'Q'), ('SA', 'NSW'), ('SA', 'WA'),
          ('SA', 'V'), ('WA', 'NT'), ('NT', 'Q'), ('Q', 'NSW'), ('NSW', 'V')]
-
-    # could also define c as a dictionary... TODO
-    C = {'SA': ['WA', 'NT', 'Q', 'NSW', 'WA', 'V'],
-         'WA': ['NT']}
 
     m_csp = CSP(x=x, d=d, c=c)
     print(m_csp.backtracking_search(csp=m_csp))
