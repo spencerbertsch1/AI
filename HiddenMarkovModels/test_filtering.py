ba64c3d9300efcacfe50e15ed0a12be4c29ed25b
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-11-13 15:47:46 -0500

    add test_filtering function

--- HiddenMarkovModels/test_filtering.py
+++ HiddenMarkovModels/test_filtering.py
@@ -1,10 +1,33 @@
+# Spencer Bertsch
+# November 2021
+# Assignment 6
+# CS 276 @ Dartmouth College
 
-# imports
 from Filtering import HMM
+import random
+random.seed(4)
+
+
+def test_filtering(starting_state: tuple, path_length: int, verbose: bool, show_heatmaps: bool):
+    """
+    Function to test the filtering algorithm
+
+    :param starting_state: the (x, y) position of the starting state of the robot
+    :param path_length: the number of steps that the robot will take on it's path
+    :param verbose: True if you want to see lots of printed output showing the robot's journey
+    :param show_heatmaps: True if you want to see heatmaps generated showing the ground truth vs the Current State
+    :return: NA
+    """
+    h = HMM(starting_state=starting_state,
+            path_length=path_length,
+            verbose=verbose,
+            show_heatmaps=show_heatmaps)  # <-- heatmaps require matplotlib and seaborn to be installed!
+    h.pretty_print_maze(matrix=h.maze, maze_name='Maze')
+    h.filtering()
 
 
 if __name__ == "__main__":
-
-    h = HMM(starting_state=(0, 0), path_length=15, verbose=True)
-    h.pretty_print_maze(matrix=h.maze, maze=True)
-    # h.generate_path()
+    test_filtering(starting_state=(0, 0),
+                   path_length=12,
+                   verbose=True,
+                   show_heatmaps=False)
